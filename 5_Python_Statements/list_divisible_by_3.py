70bd6f30a87e0e51bdcb74e590c0da6dc56f7a3e
davidcoxch <davidcoxch@gmail.com>
2020-08-09 10:55:48 +0200

    correct directoty name

---
+++ 5_Python_Statements/list_divisible_by_3.py
@@ -0,0 +1,6 @@
+'''
+Use a List Comprehension to create a list of all numbers between 1 and 50 that are divisible by 3.
+'''
+
+y = [x for x in range(1,51) if x%3 == 0]
+print(y)
---
+++ 5_Python_Statements/print_even_numbers.py
@@ -0,0 +1,4 @@
+'''
+Use range() to print all the even numbers from 0 to 10.
+'''
+print(list(range(0,11,2)))
